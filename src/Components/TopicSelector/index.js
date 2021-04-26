b238f61938e16c9c8b8908ba95475dbf04fd1e6f
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-04-26 12:19:47 +0100

    Add form styling

--- src/Components/TopicSelector/index.js
+++ src/Components/TopicSelector/index.js
@@ -8,22 +8,35 @@ const TopicSelector = () => {
         <>
             <div id="topic-container">
                 <form action="" method="get">
-                    <label htmlFor= "difficulties">Select Difficulty: </label>
-                    <select id="difficulties" name="difficulties">
-                        <option value="Easy">Easy</option>
-                        <option value="Medium">Medium</option>
-                        <option value="Hard">Hard</option>
-                    </select>
+                    <div class="col-25">
+                        <label htmlFor= "difficulties">Select Difficulty:</label>
+                    </div>
+                        <div class="options">
+                            <select id="difficulties" name="difficulties">
+                                <option value="Easy">Easy</option>
+                                <option value="Medium">Medium</option>
+                                <option value="Hard">Hard</option>
+                            </select>
+                        </div>
+                    
+                    
                     <br></br>
-                    <label htmlFor= "questions">Number of Questions: </label>
-                    <select id="questions" name="questions">
-                        <option value="5">5</option>
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                    </select>
-                    <input type="submit"/>
+                    <div class="col-25">
+                        <label htmlFor= "questions">Number of Questions:</label>
+                    </div>
+                            <div class="options">
+                                <select id="questions" name="questions">
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="20">20</option>
+                                </select>
+                            </div>
+                            
+                    
                 </form>
                 <TopicCards />
+                <br></br>
+                <input type="submit"/>
             </div>
         </>
     )
