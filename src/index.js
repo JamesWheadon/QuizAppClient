d921cafa24406f74ebe3dd0cb67fa0ded11d7e3d
James Wheadon <james.wheadon@yahoo.com>
2021-04-26 09:58:57 +0100

    initialise websocket

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000/");
+
 
 ReactDOM.render(
     <React.StrictMode>
