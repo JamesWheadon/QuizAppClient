9f912421b723f4b08820e2206e42073dec07e4d1
James Wheadon <james.wheadon@yahoo.com>
2021-04-27 09:59:02 +0100

    fix few errors stopping run dev

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import questionsReducer from './reducers/questions'
+import questionsReducer from './reducers/questionsReducer'
 const store = createStore(questionsReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
