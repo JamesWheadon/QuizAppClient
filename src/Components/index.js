e72454c90d2c746d8bfe2580cfbf9e8c03e38628
James Wheadon <james.wheadon@yahoo.com>
2021-04-27 15:06:59 +0100

    remove file

--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as TopicSelector} from './TopicSelector'
-export {default as TopicCard} from './TopicCard'
-
-
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as NavBar } from './NavBar';
-export { default as BackButton } from './BackButton';
-export { default as UsernameRoom } from './UsernameRoom';
-export { default as RoomUser } from './RoomUser';
-export { default as RoomChat } from './RoomChat';
-export { default as ChatMessage } from './ChatMessage';
