d5b0df5f2cace1686c34b2e3214be5c5c7b7ac7d
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-04-27 14:11:02 +0100

    Fix image size

--- src/Components/TopicCard/index.js
+++ src/Components/TopicCard/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import './style.css'
 
 const TopicCard = ({ topic, selectCard }) => {
     
     return (
         <>
-            <div class="card" id={topic.id}>
+            <div className="card" id={topic.id}>
                 <img className="topics-img" src={topic.image} onClick= {()=>selectCard(topic.id)} />
                 <div className="container">
                     <h3><b>{topic.name}</b></h3>
