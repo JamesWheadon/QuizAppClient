c323735544e78d5d3417656f933102eab941ec26
James Wheadon <james.wheadon@yahoo.com>
2021-04-27 10:33:36 +0100

    change test for RoomChat so doesnt mock ChatNessage and passes

--- src/components/RoomChat/RoomChat.test.js
+++ src/components/RoomChat/RoomChat.test.js
@@ -1,14 +1,14 @@
 import { default as RoomChat } from '.';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 describe('RoomChat', () => {
-    <ChatMessage /> = jest.fn()
     beforeEach(() => {
         render(<RoomChat messages={[{username: "Steve", message: "Can"}]}/>);
     });
 
     test('it calls ChatMessage with username Steve and message Can', () => {
-        expect(<ChatMessage />).toHaveBeenCalled();
-        expect(<ChatMessage />).toHaveBeenCalledWith(username="Steve", message="Can");
+        let message = screen.getByRole('message');
+        expect(message).toBeInTheDocument();
+        expect(message.textContent).toBe('Steve: Can');
     });
 });
