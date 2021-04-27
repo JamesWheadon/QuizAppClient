45246f86a40a940f003fc33eaea5065e42f96483
James Wheadon <james.wheadon@yahoo.com>
2021-04-27 15:26:22 +0100

    remove additional export from componenets index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,4 @@ export { default as RoomChat } from './RoomChat';
 export { default as RoomUser } from './RoomUser';
 export { default as TopicCard } from './TopicCard';
 export { default as TopicSelector } from './TopicSelector';
-export { default as UsernameRoom } from './UsernameRoom'; 
-export { default as TopicSelector } from './TopicSelector'; 
+export { default as UsernameRoom } from './UsernameRoom';
