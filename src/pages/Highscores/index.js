2ca7c13e08d0e35cdd8df634caa520fc79fa4c1e
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-27 15:59:21 +0100

    refactor tests using new leaderboard component

--- src/pages/Highscores/index.js
+++ src/pages/Highscores/index.js
@@ -1,54 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./styles.css";
 import axios from "axios";
 
+import { Leaderboard } from "../../components";
+
 const Highscores = () => {
+
     const [userData, setUserData] = useState("");
+
+    useEffect(() => {
+        async function getUserData() {
+            try {
+                const { data } = await axios.get(
+                    `http://localhost:3000/users`
+                )
+                setUserData(data)
+            } catch {
+                setUserData();
+            }
+        }
+        getUserData()
+    }, []);
 
     const handleUsers = (event) => {
         event.preventDefault();
     }
-
-    async function getUserData() {
-        try {
-            const { data } = await axios.get(
-                `http://localhost:3000/users`
-            )
-            setUserData(data)
-        } catch {
-            return (
-                <div>Oops, no highscores yet!</div>
-            )
-        }
-    }
-
     return (
-        <div className="home-container" onLoad={getUserData}>
+        <div className="home-container">
             <div className="highscores-container">
-                <div className="highscores-text">Leaderboard</div>
-                <br/>
-                <br/>
-                <div className="highscores-point">
-                    <table className="highscores-table">
-                        <tbody>
-                            <tr className="table-input">
-                                <th>Nickname</th>
-                                <th>Score</th>
-                                <th>Icon</th>
-                            </tr>
-                            {userData ? userData.map((g) => (
-                                <tr>
-                                    <td>{g.name}</td>
-                                    <td>{g.highscore}</td>
-                                    <td>whale</td>
-                                </tr>)) : "" }
-                        </tbody>
-                    </table>
-                </div>
-                <img className="highscore-img" src="../../images/wizard-two.png" alt=""/>
+                <br />
+                <br />
+                {userData 
+                ? <Leaderboard data={userData} /> 
+                : <div><p>Oops, no highscores yet!</p></div>
+                }
+                <img className="highscore-img" src="../../images/wizard-two.png" alt="wizard" />
             </div>
         </div>
     )
 }
 
-export default Highscores ; 
+export default Highscores;
---
+++ src/pages/Highscores/test.js
@@ -0,0 +1,39 @@
+import { prettyDOM, screen } from '@testing-library/react';
+import Highscores from '.';
+
+const API_URL = "http://localhost:3000";
+
+import axios from 'axios';
+jest.mock('axios');
+
+describe('Highscores', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
+
+    test('it renders with a table on successful api call', async () => {
+        axios.get.mockResolvedValue({ data: [{ id: 1, name: 'Ryan', highscore: 100 }] });
+
+        render(<Highscores />);
+
+        const table = await screen.findByRole('table');
+        expect(table).toBeInTheDocument()
+
+    })
+
+    test('it renders with an error on failed API call', async () => {
+        axios.get.mockRejectedValue(new Error(''))
+        render(<Highscores />);
+
+        const userError = await screen.findByText("Oops, no highscores yet!")
+        expect(userError).toBeInTheDocument()
+    })
+
+    test('it renders with an image', async () => {
+        axios.get.mockResolvedValue({ data: [{ id: 1, name: 'Ryan', highscore: 100 }] });
+        render(<Highscores />);
+
+        expect(await screen.findByAltText("wizard", {exact: false})).toBeInTheDocument();
+    })
+
+})
