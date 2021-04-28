dc83752ee3bb86c7ac1d0247e6b74f78de748cb8
mfy <Faisaly1234@outlook.com>
2021-04-28 15:22:49 +0100

    added icon to reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ const setUser = (name, id, highscore, score=0) => ( { type: 'LOAD_USER', payload
 const setPlayerScores = (arr) => ( { type: 'ADD_PLAYERS', payload: arr } );
 
 export const setPlayer1 = () => ( { type: 'SET_PLAYER1', payload: { player1: true } } );
+export const setIcon = (ref) => ( { type: 'LOAD_ICON', payload: ref } );
 export const updatePlayerScore = () => ({ type: 'UPDATE_SCORE' })
 
 
