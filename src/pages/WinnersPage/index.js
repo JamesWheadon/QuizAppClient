4dad89d5cf5ab7596575740934b55eda6848f95e
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-29 10:02:34 +0100

    add mock player data to leaderboard

--- src/pages/WinnersPage/index.js
+++ src/pages/WinnersPage/index.js
@@ -3,11 +3,19 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getAllPlayers } from '../../actions';
 import { Leaderboard } from "../../components";
 
-const WinnersPage = ({ finished }) => {
+const WinnersPage = ({ users, finished }) => {
 
     const dispatch = useDispatch();
     // const [userData, setUserData] = useState("");
-    let playersData = useSelector(state => state.players);
+    // let playersData = useSelector(state => state.players);
+    // let playersData = users;
+    // let playersData = [
+    //     {id: 1, name: "test", score: 5, icon: 1},
+    //     {id: 2, name: "lkdfmsdlf", score: 5, icon: 2},
+    //     {id: 2, name: "ndnfkdf", score: 5, icon: 3},
+    //     {id: 2, name: "lkfmdsd", score: 5, icon: 4},
+    //     {id: 2, name: "dkjfnbhsdkjifn", score: 5, icon: 5},
+    // ]
     const quiz = useSelector(state => state.quiz);
     
     playersData = playersData.map(p => {return {...p, highscore: p.score/quiz.length}})
@@ -23,11 +31,10 @@ const WinnersPage = ({ finished }) => {
                 <br />
                 {finished
                     ?
-                    <Leaderboard data={playersData} />
-                    : <div><p>Waiting for others...</p></div>
+                    <Leaderboard data={playersData} showIcons={true}/>
+                    : <div><p>Waiting for others to finish...</p></div>
                 }
-                <img className="highscore-img" src="https://media.tenor.com/images/d894ea3155542ede777f6edf39a5f8ea/tenor.gif" alt="winners" />
-                <img src="" alt="" />
+                {/* <img className="highscore-img" src="https://media.tenor.com/images/d894ea3155542ede777f6edf39a5f8ea/tenor.gif" alt="winners" /> */}
             </div>
         </div>
     )
