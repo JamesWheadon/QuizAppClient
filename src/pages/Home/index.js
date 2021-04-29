80c222a451d0782d87fd2cdafb5403a6748145c3
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-29 14:09:08 +0100

    add in new logo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,13 +6,18 @@ const Home = () => {
 
     return (
         <div className="home-container">
+
+                <img className="main-logo" src="/images/logo.svg" alt="" />    
+
+            {/* <img className="main-logo" src="../../images/Quizards-logo-small.png" alt="" /> */}
             <div className="welcome-text">
-                <h1>Welcome to Quizards!</h1>
-                {/* <button className="home-button">Start A Quiz</button> */}
-
-
+                {/* <h1>Welcome to Quizards!</h1> */}
+                
+                <p>
+                Compete in a magical quizzing duel against your rival quizards to become the king of all wizards and win the coveted crown prize!
+                </p>
             </div>
-            <img className="main-logo" src="../../images/Quizards-logo-small.png" alt="" />
+            
             <Link to="/startquiz">
                 <button type="button" className="start-quiz-btn">
                     Start a Quiz!
