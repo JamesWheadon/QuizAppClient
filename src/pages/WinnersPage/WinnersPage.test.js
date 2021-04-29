4681726a34a73ee9f9c9cad85871a1706bb1aba5
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-29 10:49:18 +0100

    refactor tests

--- src/pages/WinnersPage/WinnersPage.test.js
+++ src/pages/WinnersPage/WinnersPage.test.js
@@ -1,38 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import WinnersPage from '.';
 
-import axios from 'axios';
-jest.mock('axios');
-
 describe('Winners page', () => {
-    let data = [{ id: 1, name: 'Ryan', highscore: 1 }];
+    let table;
+    let players;
+
+    let initState = {
+        questions: [{ question: "", correct_answer: "", incorrect_answers: [] }],
+        quiz: { id: 1, length:10 },
+        user: { name: "Steve", id: 0, score: 0, highscore: 0, icon: "" },
+        players: [{ id: 1, name: 'Ryan', score: 1, icon: "1" }],
+        player1: false,
+        loading: false
+    };
 
     beforeEach(() => {
-        render(<Leaderboard data={data} />);
         jest.resetAllMocks();
     })
 
-    test('it renders with a table on successful api call', async () => {
-        axios.get.mockResolvedValue({ data: [{ id: 1, name: 'Ryan', highscore: 100 }] });
+    it('should map all the lobby users into a table with their name, score and icon', async () => {
+        renderWithReduxProvider(<WinnersPage users={initState.players} finished={true} />, { initState });
 
-        render(<WinnersPage />)
+        table = await screen.findByRole('table');
+        players = await within(table).findAllByRole('row');
 
-        const table = await screen.findByRole('table');
         expect(table).toBeInTheDocument()
-    })
+        expect(players).toHaveLength(2); 
 
-    test('it renders with an error on failed API call', async () => {
-        axios.get.mockRejectedValue(new Error(''))
-        render(<WinnersPage />);
+        const playerName = await within(players[1]).getByText('Ryan');
+        const playerScore = await within(players[1]).getByText('10%');
+     
+        expect(playerName).toBeInTheDocument();
+        expect(playerScore).toBeInTheDocument();
+    });
 
-        const userError = await screen.findByText("Oops, no winner yet!")
-        expect(userError).toBeInTheDocument()
-    })
+    test('should render a message if the game is not finished', () => {
+        renderWithReduxProvider(<WinnersPage users={initState.players} finished={false} />, { initState });
+
+        expect(screen.getByText("waiting", { exact: false })).toBeInTheDocument();
+    });
 
     test('it renders with an image', async () => {
-        axios.get.mockResolvedValue({ data: [{ id: 1, name: 'Ryan', highscore: 100 }] });
-        render(<Highscores />);
-
-        expect(await screen.findByAltText("winners", {exact: false})).toBeInTheDocument();
+        renderWithReduxProvider(<WinnersPage users={initState.players} finished={true} />, { initState });
+        expect(await screen.findByAltText("winners gif", { exact: false })).toBeInTheDocument();
     })
 })
