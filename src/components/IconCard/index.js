2a94e0cf2094ae09c954973308225c17dd91e32a
James Wheadon <james.wheadon@yahoo.com>
2021-04-29 23:25:31 +0100

    larger icon when selected based on class

--- src/components/IconCard/index.js
+++ src/components/IconCard/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import './style.css';
 
 const IconCard = ({ icon, selectIcon }) => {
-    
+    const chosenIcon = useSelector(state => state.user.icon)
     return (
         <>
             <div className="icon-card" id={icon.id}>
-                <img className="icon-img" src={icon.url} onClick= {()=>selectIcon(icon.id)} tabIndex="0" alt="icons"/>
+                <img className={chosenIcon == icon.id ? "icon-img selected" : "icon-img"} src={icon.url} onClick= {()=>selectIcon(icon.id)} tabIndex="0" alt="icons"/>
             </div>
 
         </>
