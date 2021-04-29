e92f19a54bb7427f3f8e05283f1adac90ca69652
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-29 10:53:12 +0100

    remove login page

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,17 +5,13 @@ import './style.css'
 
 const NavBar = () => {
     return (
-        // <header className="nav-head">
             <nav className="nav-links">
                 {/* <img className="nav-logo" src="../../images/Quizards-logo.png" alt=""/> */}
                 <NavLink className="home-link" exact to="/">Home</NavLink>
                 <NavLink activeClassName="active" to="/startquiz">Start A Quiz</NavLink>
-                {/* <NavLink activeClassName="active" to="/about">About</NavLink>  about and how to play route */}
                 <NavLink activeClassName="active" to="/highscores">Highscores</NavLink>
-                <NavLink activeClassName="active" to="/login">Login</NavLink>
                 <BackButton />
             </nav>
-        // </header>
     )
 }
 
