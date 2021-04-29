1dfc571e7f4a04f51380ecd10ee37771d2fa14b1
mfy <Faisaly1234@outlook.com>
2021-04-29 14:36:30 +0100

    added async action tests

--- src/actions/test.js
+++ src/actions/test.js
@@ -8,10 +8,9 @@ const mockStore = configureMockStore(middlewares)
 
 jest.mock('axios');
 
-describe('question actions', () => {
+describe('Question Actions', () => {
 
-    const initState = {
-    }
+    const initState = {}
 
     const store = mockStore(initState)
 
@@ -20,26 +19,6 @@ describe('question actions', () => {
     })
 
 
-    it('Dispatches LOAD_USER action', () => {
-        // Response body sample
-        const r = 
-            {
-                id: 1,
-                name: 'bob',
-                highscore: 100,
-                score: 0
-            }
-
-        const expectedActions = [
-            { type: 'LOAD_USER', payload: r }
-        ]
-        store.dispatch(action.setUser("bob", 1, 100))
-        const actions = store.getActions()
-        console.log(actions)
-        expect(store.getActions()).toEqual(expectedActions)
-             
-    })
-
     it('Dispatches UPDATE_SCORE action', () => {
         
         const expectedActions = [
@@ -47,8 +26,127 @@ describe('question actions', () => {
         ]
         store.dispatch(action.updatePlayerScore())
         const actions = store.getActions()
-        console.log(actions)
         expect(store.getActions()).toEqual(expectedActions)
              
     })
+
+    it('Dispatches LOAD_ICON action', () => {
+        const ref = "test"
+        
+        const expectedActions = [
+            { type: 'LOAD_ICON', payload: ref }
+        ]
+        store.dispatch(action.setIcon(ref))
+        const actions = store.getActions()
+        expect(store.getActions()).toEqual(expectedActions)
+             
+    })
+
+
+    it('Dispatches SET_ERROR action', () => {
+        const err= { message:"Error" }
+        
+        const expectedActions = [
+            { type: 'SET_ERROR', payload: err.message}
+        ]
+        store.dispatch(action.error(err))
+        const actions = store.getActions()
+        expect(store.getActions()).toEqual(expectedActions)
+             
+    })
+})
+
+describe('async actions', () => {
+    let store;
+
+    beforeEach(() => {
+        store = mockStore({});
+        store.clearActions()
+    })
+
+    it('addUser', () => {
+        const r = 
+         {
+                id: 1,
+                name: 'bob',
+                highscore: 100,
+                score: 0
+            }
+
+        
+        const expectedActions = [
+            { type: 'LOAD_USER', payload: r }
+        ]
+        
+        axios.post.mockImplementationOnce(() => Promise.resolve({data:r}));
+         
+        return store.dispatch(action.addUser("bob"))
+          .then(() => { // return of async actions
+            expect(store.getActions()).toEqual(expectedActions)
+          })
+      })
+
+      it('fetchQuestions', () => {
+        const r = 
+         {
+            quiz: 1,
+            questions:  [ {question: "h", correct_answer: "j", incorrect_answers :["j"] } ]
+
+        }
+
+        
+        const expectedActions = [
+            { type: 'LOAD_QUESTIONS', payload: r }
+        ]
+        
+        axios.post.mockImplementationOnce(() => Promise.resolve({data:r}));
+         
+        return store.dispatch(action.fetchQuestions(1))
+          .then(() => { // return of async actions
+            expect(store.getActions()).toEqual(expectedActions)
+          })
+      })
+
+
+      it('sendscore', () => {
+        const r = {
+                id: 1,
+                name: 'bob',
+                highscore: 100,
+                score: 4
+            }
+
+        
+        const expectedActions = [
+            { type: 'LOAD_USER', payload: r }
+        ]
+        
+        axios.get.mockImplementationOnce(() => Promise.resolve({data: r}));
+         
+        return store.dispatch(action.sendScore(1,"bob", 4))
+          .then(() => { // return of async actions
+            console.log(store.getActions())
+            expect(store.getActions()).toEqual(expectedActions)
+          })
+      })
+
+
+      it('getAllPlayers', () => {
+        const r = ["test"]
+
+        
+        const expectedActions = [
+            { type: 'ADD_PLAYERS', payload: r }
+        ]
+        
+        axios.get.mockImplementationOnce(() => Promise.resolve({data: r}));
+         
+        return store.dispatch(action.getAllPlayers(1))
+          .then(() => { // return of async actions
+            console.log(store.getActions())
+            expect(store.getActions()).toEqual(expectedActions)
+          })
+      })
+
+
 })
