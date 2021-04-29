b7bc8e76b37561b00e04fd1b4ba85572364eaacc
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-29 17:29:24 +0100

    add send score dispatch on quiz completion

--- src/pages/WinnersPage/index.js
+++ src/pages/WinnersPage/index.js
@@ -1,32 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Leaderboard } from "../../components";
+import { sendScore } from "../../actions";
 
 const WinnersPage = ({ users, finished }) => {
-    
+    // let quiz = {id: 1, length: 10}
     // let playersData = [
-    //     {id: 1, name: "test", score: 5, icon: 1},
-    //     {id: 2, name: "lkdfmsdlf", score: 5, icon: 2},
-    //     {id: 2, name: "ndnfkdf", score: 5, icon: 3},
-    //     {id: 2, name: "lkfmdsd", score: 5, icon: 4},
-    //     {id: 2, name: "dkjfnbhsdkjifn", score: 5, icon: 5},
+    //     { id: 1, name: "test", score: 2, icon: 1 },
+    //     { id: 2, name: "lkdfmsdlf", score: 4, icon: 2 },
+    //     { id: 3, name: "ndnfkdf", score: 5, icon: 3 },
+    //     { id: 4, name: "lkfmdsd", score: 1, icon: 4 },
+    //     { id: 5, name: "dkjfnbhsdkjifn", score: 8, icon: 5 },
     // ]
-    const quiz = useSelector(state => state.quiz);
-    let playersData = users;
-    playersData = playersData.map(p => {return {...p, highscore: p.score/quiz.length}})
 
+    const dispatch = useDispatch();
+
+    // comment out these to use mock data
+    const quiz = useSelector(state => state.quiz);
+    const user = useSelector(state => state.user);
+    let playersData = users;
+    
+    playersData = playersData.map(p => { return { ...p, highscore: p.score / quiz.length } })
+
+    useEffect(() => {
+        dispatch(sendScore(quiz.id, user.name, user.score));
+    }, []);
+ 
     return (
-        <div className="home-container">
-            <div className="highscores-container">
-                <br />
-                <br />
-                {finished
-                    ?
-                    <Leaderboard data={playersData} showIcons={true}/>
-                    : <div><p>Waiting for others to finish...</p></div>
-                }
-                <img className="highscore-img" src="https://media.tenor.com/images/d894ea3155542ede777f6edf39a5f8ea/tenor.gif" alt="winners gif" />
-            </div>
+        <div className="highscores-container">
+            <br />
+            <br />
+            {finished
+                ?
+                <Leaderboard data={playersData} showIcons={true} />
+                : <div><p>Waiting for others to finish...</p></div>
+            }
+            {/* <img className="highscore-img" src="https://media.tenor.com/images/d894ea3155542ede777f6edf39a5f8ea/tenor.gif" alt="winners gif" /> */}
         </div>
     )
 }
