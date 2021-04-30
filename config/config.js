eaeddae67e4dd45614b1067b10de6d5224d443f0
Rishi B <dalek1112@yahoo.co.uk>
2021-04-30 12:07:58 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    apiUrl: " http://localhost:3000",
-    socketUrl: "http://localhost:5001"
+//     apiUrl: " http://localhost:3000",
+//     socketUrl: "http://localhost:5001"
+    apiUrl: "https://quizards-api.herokuapp.com",
+    socketUrl: "https://quizards-socket.herokuapp.com/"
 }
 
