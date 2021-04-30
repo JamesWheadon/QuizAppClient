e5a5353090e1dc121dc4372258a7fa72242230f1
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-30 12:43:28 +0100

    add music category

--- src/components/QuizForm/index.js
+++ src/components/QuizForm/index.js
@@ -64,6 +64,11 @@ const QuizForm = ({ handleFormSubmit }) => {
             id: "27",
             name: "Animals",
             image: "https://images.unsplash.com/photo-1591824438708-ce405f36ba3d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+        }, 
+        {
+            id: "12",
+            name: "Music",
+            image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
         }
     ]
     const topicsList = topics.map(topic => {
